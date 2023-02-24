c995b1ce04f73bd3de545a61d7285f910c5b133d
ivan_cheng <ivan_cheng@weleader.com.tw>
2023-02-24 16:11:17 +0800

    update message format

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def firing_alert(request):
         time = str(datetime.now().date()) + ' ' + str(datetime.now().time().strftime('%H:%M:%S'))
     header = {'Authorization':request.headers['AUTHORIZATION']}
     for alert in request.json['alerts']:
-        msg = alert['annotations']['summary']
+        msg = "\n警示訊息: " + alert['annotations']['summary']
         msg = {'message': msg}
         response = requests.post(LINE_NOTIFY_URL, headers=header, data=msg)
 
